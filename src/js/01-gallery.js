1f9790e5dd57e756e48380e48b6385e86ed1041d
OksanaV2284 <110124340+OksanaV2284@users.noreply.github.com>
2023-01-08 17:22:55 +0200

    add lodash.throttle

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(SimpleLightbox);
+
+const onGallery = document.querySelector('.gallery');
+onGallery.insertAdjacentHTML(
+  'beforeend',
+  galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li >
+      <a class="gallery__item" href='${original}'>
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source='${original}'
+          alt="${description}"
+        />
+      </a>
+    </li>`
+    )
+    .join('')
+);
+
+onGallery.addEventListener(
+  'click',
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  })
+);
+
+function onClickImg(e) {
+  e.preventDefault();
+
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
-
+import Player from '@vimeo/player';
+console.log(Player);
+var throttle = require(' lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+function foo() {
+  player.getCurrentTime().then(function (time) {
+    console.log('time:', time);
+    if (time >= 110) {
+      player.pause();
+    }
+  });
+  setTimeout(foo, 1000);
+}
+player.on('play', function () {
+  foo();
+});
