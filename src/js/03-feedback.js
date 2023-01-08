54e9bf3bd84fb9c56fce45e6872371464ec19691
OksanaV2284 <110124340+OksanaV2284@users.noreply.github.com>
2023-01-08 17:44:04 +0200

    fix libr

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.feedback-form');
+const formData = {
+  email: '',
+  password: '',
+};
+form.addEventListener('submit', listeningFormFillEvents);
+function listeningFormFillEvents(evn) {
+  evn.preventDefault();
+  const {
+    elements: { email, password },
+  } = evn.currentTarget;
+  if (!email.value || !password.value) {
+    alert('Всі поля повинні бути заповнені!');
+  }
+  formData.email = email.value;
 
+  formData.password = password.value;
+  evn.currentTarget.reset();
+  return console.log(formData);
+}
