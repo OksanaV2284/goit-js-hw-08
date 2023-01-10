13204825acfbddc7e649783958a939d352cf9d25
OksanaV2284 <110124340+OksanaV2284@users.noreply.github.com>
2023-01-10 19:43:37 +0000

    Deploying to gh-pages from @ OksanaV2284/goit-js-hw-08@cea8b48a58e1f2998ebc8ec035f8d2d335036c7b 🚀

--- 03-feedback.2106da57.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form"),r={email:"",password:""};e.addEventListener("submit",(function(e){e.preventDefault();var a=e.currentTarget.elements,t=a.email,l=a.password;t.value&&l.value||alert("Всі поля повинні бути заповнені!");return r.email=t.value,r.password=l.value,e.currentTarget.reset(),console.log(r)}))}();
-//# sourceMappingURL=03-feedback.2106da57.js.map
