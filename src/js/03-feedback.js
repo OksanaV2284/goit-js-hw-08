429c6ab6ec7d7f58ac7fadfc91eb37dbf17f7035
OksanaV2284 <110124340+OksanaV2284@users.noreply.github.com>
2023-01-12 16:50:16 +0200

    add 8/3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,44 @@
 import throttle from 'lodash.throttle';
-const KEY = 'feedback-form-state';
+const KEY_LOCAL = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
+const rest = {
+  textarea: document.querySelector('textarea'),
+  input: document.querySelector('input'),
+};
+
 const formData = {
   email: '',
   message: '',
 };
-form.addEventListener('input', listeningFormFillEvents);
-form.addEventListener('submit', listEvents);
 
-// function listeningFormFillEvents(evn) {
-//   evn.preventDefault();
-//   const {
-//     elements: { email, password },
-//   } = evn.currentTarget;
-//   if (!email.value || !password.value) {
-//     alert('Всі поля повинні бути заповнені! ');
-//   }
-//   formData.email = email.value;
+form.addEventListener('input', throttle(onTextInput, 500));
+populateTextarea();
+form.addEventListener('submit', onFormSubmit);
+function onTextInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(KEY_LOCAL, JSON.stringify(formData));
+}
 
-//   formData.password = password.value;
-//   evn.currentTarget.reset();
-//   return console.log(formData);
-// }
-function listeningFormFillEvents(evn) {
-  evn.preventDefault();
+function onFormSubmit(evt) {
+  evt.preventDefault();
   const {
     elements: { email, message },
-  } = evn.currentTarget;
-  if (email.value || password.value) {
-    localStorage.setItem(KEY_LOCAL, email.value);
+  } = evt.currentTarget;
+  if (!email.value || !message.value) {
+    alert('Всі поля повинні бути заповнені!');
   }
-  formData.email = email.value;
+  evt.currentTarget.reset();
+  localStorage.removeItem(KEY_LOCAL);
+  console.log(formData);
+}
+function populateTextarea() {
+  const savedMessege = JSON.parse(localStorage.getItem(KEY_LOCAL));
 
-  formData.password = password.value;
-  evn.currentTarget.reset();
-  return console.log(formData);
+  if (savedMessege) {
+    const dsds = JSON.stringify(savedMessege.email);
+    const mas = JSON.stringify(savedMessege.message);
+
+    rest.textarea.value = JSON.parse(mas);
+    rest.input.value = JSON.parse(dsds);
+  }
 }
